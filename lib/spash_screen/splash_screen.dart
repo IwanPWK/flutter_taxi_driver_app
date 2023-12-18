d97264e74939df54d3d4ccc98f4ce4723dc3bdaa
IwanPWK <koadhyow@gmail.com>
2023-12-18 13:35:20 +0700

    Until Vid 3. Splash Screen

---
+++ lib/spash_screen/splash_screen.dart
@@ -0,0 +1,54 @@
+import 'dart:async';
+
+import '../main_screen/main_screen.dart';
+import 'package:flutter/material.dart';
+
+class MySplashScreen extends StatefulWidget {
+  const MySplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _MySplashScreenState createState() => _MySplashScreenState();
+}
+
+class _MySplashScreenState extends State<MySplashScreen> {
+  startTimer() {
+    Timer(const Duration(seconds: 3), () async {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (c) => const MainScreen()));
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    startTimer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        color: Colors.black,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("images/logo1.png"),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Uber & inDriver Clone App",
+                style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

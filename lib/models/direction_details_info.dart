44293132d9af482d0373a514f4a632725cfd2401
IwanPWK <koadhyow@gmail.com>
2023-12-27 21:42:03 +0700

    Get driver current location logic

---
+++ lib/models/direction_details_info.dart
@@ -0,0 +1,15 @@
+class DirectionDetailsInfo {
+  int? distanceValue;
+  int? durationValue;
+  String? ePoints;
+  String? distanceText;
+  String? durationText;
+
+  DirectionDetailsInfo({
+    this.distanceText,
+    this.durationValue,
+    this.ePoints,
+    this.distanceValue,
+    this.durationText,
+  });
+}
--- lib/tab_pages/home_tab.dart
+++ lib/tab_pages/home_tab.dart
@@ -2,6 +2,9 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+
+import '../assistants/assistant_methods.dart';
 
 class HomeTabPage extends StatefulWidget {
   const HomeTabPage({super.key});
@@ -13,6 +16,7 @@ class HomeTabPage extends StatefulWidget {
 class _HomeTabPageState extends State<HomeTabPage> {
   GoogleMapController? newGoogleMapController;
   final Completer<GoogleMapController> _controllerGoogleMap = Completer();
+  Position? driverCurrentPosition;
 
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
@@ -185,6 +189,20 @@ class _HomeTabPageState extends State<HomeTabPage> {
                 ''');
   }
 
+  locateDriverPosition() async {
+    Position cPosition = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    driverCurrentPosition = cPosition;
+
+    LatLng latLngPosition = LatLng(driverCurrentPosition!.latitude, driverCurrentPosition!.longitude);
+
+    CameraPosition cameraPosition = CameraPosition(target: latLngPosition, zoom: 14);
+
+    newGoogleMapController!.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+
+    String humanReadableAddress = await AssistantMethods.searchAddressForGeographicCoOrdinates(driverCurrentPosition!, context);
+    print("this is your address = " + humanReadableAddress);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -199,6 +217,7 @@ class _HomeTabPageState extends State<HomeTabPage> {
 
             //black theme google map
             blackThemeGoogleMap();
+            locateDriverPosition();
           },
         ),
       ],
